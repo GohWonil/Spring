fdb429ebcb19e13e25d68e63adfbb2bba886e2b7
kwi5124 <kwi5124@naver.com>
2023-12-08 10:19:37 +0900

    프록시 AOP, 캐시값 사용

--- 3. spring/spring_study/exan05/src/main/java/config/AppCtx.java
+++ 3. spring/spring_study/exan05/src/main/java/config/AppCtx.java
@@ -3,11 +3,21 @@ package config;
 import aopex.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@Configuration
+@Configuration // 객체 컨테이너
+@EnableAspectJAutoProxy //프록시 설정 활성화
 public class AppCtx {
-  @Bean
-  public Calculator calculator(){
+  @Bean //객체
+  public Calculator calculator() {
     return new RecCalculator();
+  }
+  @Bean
+  public ProxyCache proxyCache(){
+    return new ProxyCache();
+  }
+  @Bean //객체
+  public ProxyCalculator proxyCalculator(){
+    return new ProxyCalculator();
   }
 }
---
+++ 3. spring/spring_study/exan05/src/main/java/config/ProxyCache.java
@@ -0,0 +1,33 @@
+package config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+public class ProxyCache {
+  private Map<Long, Object> cacheData = new HashMap<>();
+  @Pointcut("execution(* aopex..*(long))")
+  public void publicTarget(){
+
+  }
+  @Around("publicTarget()")
+  public Object process(ProceedingJoinPoint joinPoint) throws Throwable{
+    Object[] args = joinPoint.getArgs(); //매개변수로 투입된 이자 값(예 - 10L)
+    Long num = (Long) args[0];
+    if(cacheData.containsKey(num)){
+      System.out.println("캐시값 사용");
+      return cacheData.get(num);
+    }
+    Object result = joinPoint.proceed();
+    //캐시 저장
+    cacheData.put(num, result);
+    System.out.println("캐시저장");
+
+    return result;
+  }
+}
