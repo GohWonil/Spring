625e66be91964446871b1d6634e6395af600ed15
kwi5124 <kwi5124@naver.com>
2023-12-14 12:03:35 +0900

    타임리프

--- 3. spring/spring_study/exam07/src/main/java/controllers/member/MemberController.java
+++ 3. spring/spring_study/exam07/src/main/java/controllers/member/MemberController.java
@@ -1,14 +1,31 @@
 package controllers.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import models.member.Member;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MemberController {
   @GetMapping("/member/join")
-  public String join(){
+  public String join(Model model){
+    Member member = Member.builder()
+        .userNo(1L)
+        .userPw("1234")
+        .userId("user01")
+        .userNm("<h1>사용자01</h1>")
+        .email("user01@test.org")
+        .regDt(LocalDateTime.now())
+        .build();
+
+    model.addAttribute("member", member);
+    model.addAttribute("pageTitle","회원가입");
     return "member/join";
   }
   @PostMapping("/member/join")
@@ -26,4 +43,23 @@ public class MemberController {
     System.out.println(form);
    return "member/login";
   }
+  @GetMapping("/member/list")
+  public String members(Model model){
+    List<Member> members = new ArrayList<>();
+    for(int i = 1; i <= 10; i++){
+      Member member = Member.builder()
+          .userNo(Long.valueOf(i))
+          .userPw("1234")
+          .userId("user" + i)
+          .userNm("사용자" + i)
+          .email("user" + i + "@test.org")
+          .regDt(LocalDateTime.now())
+          .build();
+      members.add(member);
+    }
+    model.addAttribute("members",members);
+
+    return "member/list";
+  }
+
 }
