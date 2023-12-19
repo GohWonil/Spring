56a5490cdafd6b7acb27d65babf9b7f91070031f
kwi5124 <kwi5124@naver.com>
2023-12-19 17:02:48 +0900

    스트림

--- 4.java/streamex/src/exam03/Ex01.java
+++ 4.java/streamex/src/exam03/Ex01.java
@@ -3,7 +3,7 @@ package exam03;
 import java.util.stream.IntStream;
 
 public class Ex01 {
-  public static void main(String[] args) {
-    IntStream.rangeClosed(1, 10).skip(5).forEach(System.out::println);
-  }
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 10).skip(5).forEach(System.out::println);
+    }
 }
--- 4.java/streamex/src/exam03/Ex02.java
+++ 4.java/streamex/src/exam03/Ex02.java
@@ -3,8 +3,8 @@ package exam03;
 import java.util.stream.IntStream;
 
 public class Ex02 {
-  public static void main(String[] args) {
-    int total = IntStream.rangeClosed(1, 100).filter(x -> x % 2 == 1).sum();
-    System.out.println(total);
-  }
+    public static void main(String[] args) {
+        int total = IntStream.rangeClosed(1, 100).filter(x -> x % 2 == 1).sum();
+        System.out.println(total);
+    }
 }
